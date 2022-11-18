a36f076514d079869ddab30e4bb89fab08aec1d1
Andrija Petrovic <andrija.hers@gmail.com>
2022-11-18 12:21:12 +0100

    Slowly changing the lib structure, introduced placeholders for NodeJS/browser specifics, lib.now() being the most important

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
-var 
+var theLib = {},
   jsonschema = require('jsonschema'),
   checkftions = require('allex_checkslowlevellib'),
   cleanftions = require('allex_destructionlowlevellib')(checkftions.isFunction, checkftions.isArray, checkftions.isNumber, checkftions.isString),
   inherit = require('allex_inheritlowlevellib'),
   slist = require('allex_singlelinkedlistlowlevellib')(inherit.inherit),
-  maclib = require('allex_macaddresslowlevellib')(checkftions.isFunction, checkftions.isArray),
   stringmanip = require('allex_stringmanipulationlowlevellib')(checkftions.isString, checkftions.isNull),
   objmanip = require('allex_objectmanipulationlowlevellib')(checkftions),
   functionmanip = require('allex_functionmanipulationlowlevellib')(inherit.inherit),
@@ -16,13 +15,13 @@ var
   DList = require('allex_doublelinkedlistlowlevellib')(dlinkedlistbase, inherit.inherit),
   fifo = require('allex_fifolowlevellib')(dlinkedlistbase, inherit.inherit),
   stringbuffer = require('allex_stringbufferlowlevellib')(fifo),
-  timeout = require('allex_timeoutlowlevellib')(checkftions.isFunction, fifo),
+  timeout = require('allex_timeoutlowlevellib')(checkftions.isFunction, fifo, theLib),
   eventemitter = require('allex_eventemitterlowlevellib')(dlinkedlistbase, inherit.inherit, checkftions.isFunction, checkftions.isArrayOfFunctions),
   destroyables = require('allex_destroyablemixinslowlevellib')(inherit, functionmanip.dummyFunc, eventemitter),
   props = require('allex_propertymixinslowlevellib')(inherit, functionmanip.dummyFunc, eventemitter, objmanip.extend, destroyables.Destroyable, jsonschema, stringmanip.readPropertyFromDotDelimitedString, checkftions.isFunction, checkftions.isArray, stringmanip.writePropertyFromDelimitedString),
   q = require('allex_qlowlevellib')(timeout.runNext, checkftions.isArray, checkftions.isFunction, inherit.inherit, functionmanip.dummyFunc, eventemitter),
   deferfifo = require('allex_deferfifolowlevellib')(dlinkedlistbase, inherit.inherit, q),
-  uidlib = require('allex_uidlowlevellib')(q, maclib.getMac),
+  uidlib = require('allex_uidlowlevellib')(q, theLib),
   avltreelib = require('allex_avltreelowlevellib')(dlinkedlistbase, inherit.inherit),
   map = require('allex_maplowlevellib')(avltreelib, inherit.inherit, slist),
   defermap = require('allex_defermaplowlevellib')(map, q),
@@ -31,109 +30,108 @@ var
   DIContainer = require('allex_dicontainerlowlevellib')(map, defermap, listenablemap, q, qlib, cleanftions.containerDestroyAll);
 
 var shouldClose = new eventemitter();
-function onSignal (signal) {
-  if (checkftions.isString(signal)) {
-    shouldClose.fire(new AllexError(signal, 'Server stopped'));
-    return;
-  }
-  shouldClose.fire();
-}
-function onExit (error) {
-  if (error) {
-    shouldClose.fire(error);
-    return;
-  }
-  shouldClose.fire();
-}
-process.on('SIGINT',onSignal);
-process.on('SIGTERM',onSignal);
-process.on('exit',onExit);
 
-var toExport = {
-  jsonschema : jsonschema,
-  Error: AllexError,
-  JSONizingError: AllexJSONizingError,
-  NotAnAllexErrorError: NotAnAllexErrorError,
-  dummyFunc:functionmanip.dummyFunc,
-  functionCopy:functionmanip.functionCopy,
-  traverseShallow:objmanip.traverseShallow,
-  traverseShallowConditionally:objmanip.traverseShallowConditionally,
-  traverse:objmanip.traverse,
-  traverseConditionally:objmanip.traverseConditionally,
-  extend:objmanip.extend,
-  extendWithConcat:objmanip.extendWithConcat,
-  extendShallow:objmanip.extendShallow,
-  pick:objmanip.pick,
-  pickExcept:objmanip.pickExcept,
-  inherit:inherit.inherit,
-  inheritMethods:inherit.inheritMethods,
-  isFunction:checkftions.isFunction,
-  isArray:checkftions.isArray,
-  isUndef:checkftions.isUndef,
-  defined:checkftions.defined,
-  isString:checkftions.isString,
-  isNumber:checkftions.isNumber,
-  isNull: checkftions.isNull,
-  isNotNull:checkftions.isNotNull,
-  isDefinedAndNotNull:checkftions.isDefinedAndNotNull,
-  isBoolean: checkftions.isBoolean,
-  isVal:checkftions.isVal,
-  isEqual: checkftions.isEqual,
-  has: checkftions.has,
-  isInteger : checkftions.isInteger,
-  Destroyable: destroyables.Destroyable,
-  SimpleDestroyable: destroyables.SimpleDestroyable,
-  ComplexDestroyable: destroyables.ComplexDestroyable,
-  Gettable: props.Gettable,
-  Settable: props.Settable,
-  Changeable: props.Changeable,
-  Listenable: props.Listenable,
-  ChangeableListenable: props.ChangeableListenable,
-  CLDestroyable: props.CLDestroyable,
-  Configurable: props.Configurable,
-  CBMapable: props.CBMapable,
-  prependToString: stringmanip.prependToString,
-  thousandSeparate : stringmanip.thousandSeparate,
-  readPropertyFromDotDelimitedString: stringmanip.readPropertyFromDotDelimitedString,
-  writePropertyFromDotDelimitedString: stringmanip.writePropertyFromDelimitedString,
-  toIndentedJson : stringmanip.toIndentedJson,
-  joinStringsWith : stringmanip.joinStringsWith,
-  querystring : stringmanip.querystring,
-  getMac: maclib.getMac,
-  isMac: maclib.isMac,
-  initUid: uidlib.init,
-  uid: uidlib.uid,
-  setImmediate: timeout.setImmediate,
-  clearImmediate: timeout.clearImmediate,
-  runNext: timeout.runNext,
-  destroyASAP: timeout.destroyASAP,
-  clearTimeout: timeout.clearTimeout,
-  intervals: timeout.intervals,
-  DList: DList,
-  Fifo: fifo,
-  StringBuffer: stringbuffer,
-  DeferFifo: deferfifo,
-  DeferMap : defermap,
-  Map: map,
-  SortedList: slist,
-  arryDestroyEl: cleanftions.arryDestroyEl,
-  arryDestroyAll : cleanftions.arryDestroyAll,
-  arryNullEl: cleanftions.arryNullEl,
-  arryNullAll:cleanftions.arryNullAll,
-  objNullAll: cleanftions.objNullAll,
-  objDestroyAll: cleanftions.objDestroyAll,
-  containerDestroyAll: cleanftions.containerDestroyAll,
-  containerDestroyDeep: cleanftions.containerDestroyDeep,
-  HookCollection: eventemitter,
-  q: q,
-  qlib: qlib,
-  isIdentical: require('allex_isidenticallowlevellib')(objmanip.traverseShallowConditionally),
-  ListenableMap: listenablemap,
-  request: require('allex_httprequestlowlevellib')(objmanip.traverseShallow, checkftions.isFunction, functionmanip.dummyFunc),
-  DIContainer : DIContainer,
-  moduleRecognition: require('allex_modulerecognitionlowlevellib')(checkftions.isString, checkftions.isFunction, q, qlib),
-  capitalize : stringmanip.capitalize,
-  shouldClose : shouldClose
-};
+function getMac () {
+  return '';
+}
+function isMac () {
+  return true;
+}
+function pid () {
+  return 0;
+}
+function exit (code) {}
 
-module.exports = toExport;
+function now () {
+  return Date.now();
+}
+
+theLib.jsonschema = jsonschema;
+theLib.Error= AllexError;
+theLib.JSONizingError= AllexJSONizingError;
+theLib.NotAnAllexErrorError= NotAnAllexErrorError;
+theLib.dummyFunc=functionmanip.dummyFunc;
+theLib.functionCopy=functionmanip.functionCopy;
+theLib.traverseShallow=objmanip.traverseShallow;
+theLib.traverseShallowConditionally=objmanip.traverseShallowConditionally;
+theLib.traverse=objmanip.traverse;
+theLib.traverseConditionally=objmanip.traverseConditionally;
+theLib.extend=objmanip.extend;
+theLib.extendWithConcat=objmanip.extendWithConcat;
+theLib.extendShallow=objmanip.extendShallow;
+theLib.pick=objmanip.pick;
+theLib.pickExcept=objmanip.pickExcept;
+theLib.inherit=inherit.inherit;
+theLib.inheritMethods=inherit.inheritMethods;
+theLib.isFunction=checkftions.isFunction;
+theLib.isArray=checkftions.isArray;
+theLib.isUndef=checkftions.isUndef;
+theLib.defined=checkftions.defined;
+theLib.isString=checkftions.isString;
+theLib.isNumber=checkftions.isNumber;
+theLib.isNull= checkftions.isNull;
+theLib.isNotNull=checkftions.isNotNull;
+theLib.isDefinedAndNotNull=checkftions.isDefinedAndNotNull;
+theLib.isBoolean= checkftions.isBoolean;
+theLib.isVal=checkftions.isVal;
+theLib.isEqual= checkftions.isEqual;
+theLib.has= checkftions.has;
+theLib.isInteger = checkftions.isInteger;
+theLib.Destroyable= destroyables.Destroyable;
+theLib.SimpleDestroyable= destroyables.SimpleDestroyable;
+theLib.ComplexDestroyable= destroyables.ComplexDestroyable;
+theLib.Gettable= props.Gettable;
+theLib.Settable= props.Settable;
+theLib.Changeable= props.Changeable;
+theLib.Listenable= props.Listenable;
+theLib.ChangeableListenable= props.ChangeableListenable;
+theLib.CLDestroyable= props.CLDestroyable;
+theLib.Configurable= props.Configurable;
+theLib.CBMapable= props.CBMapable;
+theLib.prependToString= stringmanip.prependToString;
+theLib.thousandSeparate = stringmanip.thousandSeparate;
+theLib.readPropertyFromDotDelimitedString= stringmanip.readPropertyFromDotDelimitedString;
+theLib.writePropertyFromDotDelimitedString= stringmanip.writePropertyFromDelimitedString;
+theLib.toIndentedJson = stringmanip.toIndentedJson;
+theLib.joinStringsWith = stringmanip.joinStringsWith;
+theLib.querystring = stringmanip.querystring;
+theLib.getMac= getMac;
+theLib.isMac= isMac;
+theLib.initUid= uidlib.init;
+theLib.uid= uidlib.uid;
+theLib.setImmediate= timeout.setImmediate;
+theLib.clearImmediate= timeout.clearImmediate;
+theLib.runNext= timeout.runNext;
+theLib.destroyASAP= timeout.destroyASAP;
+theLib.clearTimeout= timeout.clearTimeout;
+theLib.intervals= timeout.intervals;
+theLib.DList= DList;
+theLib.Fifo= fifo;
+theLib.StringBuffer= stringbuffer;
+theLib.DeferFifo= deferfifo;
+theLib.DeferMap = defermap;
+theLib.Map= map;
+theLib.SortedList= slist;
+theLib.arryDestroyEl= cleanftions.arryDestroyEl;
+theLib.arryDestroyAll = cleanftions.arryDestroyAll;
+theLib.arryNullEl= cleanftions.arryNullEl;
+theLib.arryNullAll=cleanftions.arryNullAll;
+theLib.objNullAll= cleanftions.objNullAll;
+theLib.objDestroyAll= cleanftions.objDestroyAll;
+theLib.containerDestroyAll= cleanftions.containerDestroyAll;
+theLib.containerDestroyDeep= cleanftions.containerDestroyDeep;
+theLib.HookCollection= eventemitter;
+theLib.q= q;
+theLib.qlib= qlib;
+theLib.isIdentical= require('allex_isidenticallowlevellib')(objmanip.traverseShallowConditionally);
+theLib.ListenableMap= listenablemap;
+theLib.request= require('allex_httprequestlowlevellib')(objmanip.traverseShallow, checkftions.isFunction, functionmanip.dummyFunc);
+theLib.DIContainer = DIContainer;
+theLib.moduleRecognition= require('allex_modulerecognitionlowlevellib')(checkftions.isString, checkftions.isFunction, q, qlib);
+theLib.capitalize = stringmanip.capitalize;
+theLib.shouldClose = shouldClose;
+theLib.now = now;
+theLib.pid = pid;
+theLib.exit = exit;
+
+module.exports = theLib;
